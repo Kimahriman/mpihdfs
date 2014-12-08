84d16d8a93475eb51d4fbc2a37a424a9e9896e0a
Luna Xu <xuluna@ib166.(none)>
2014-12-07 20:47:49 -0500

    Fixed writebench to work on non-HDFS

--- hs.sh
+++ hs.sh
@@ -3,12 +3,13 @@
 
 file="hyogi-result.txt"
 
-for i in `seq 10`; do
+#for i in `seq 10`; do
 	for try in `seq 5`; do
 		echo | tee -a $file
-		echo "# n=$i, try=$try" | tee -a $file
+		echo "# n=1, try=$try" | tee -a $file
 		echo "# command: ./mpihdfs.sh mpiexec -n $i -hosts n165,ib166 ./bench /mnt/hdfs/onegig 64000000" | tee -a $file
-		./mpihdfs.sh mpiexec -n $i -hosts n165,ib166 ./bench hdfs://n165:9099/onegig 64000000 | tee -a $file
+		./mpihdfs.sh mpiexec -n 1 ./benchwrite hdfs://n166:9099/testluna$try 1073741824 64000000 | tee -a $file
+		#mpiexec -n 1 ./writebench /mnt/lunatest$try 1073741824 64000000 | tee -a $file
 	done
-done
+#done
 
